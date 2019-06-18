6302de1ad815743e9854062c988ea6257d84b218
name <email>
2019-06-18 11:42:45 +0800

    配置中心文件内容，支持加密方式，其中"app-dev.yml"中，"hello"字段为加密内容

--- app/src/main/java/com/example/spring/cloud/app/HelloController.java
+++ app/src/main/java/com/example/spring/cloud/app/HelloController.java
@@ -2,6 +2,7 @@ package com.example.spring.cloud.app;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +13,9 @@ public class HelloController {
 
     @Autowired
     HelloFeignService helloFeignService;
+
+    @Value("${hello}")
+    String hello;
 
     @RequestMapping("/hello")
     public String Hello() {
@@ -26,7 +30,7 @@ public class HelloController {
 
     @RequestMapping("/api-feign/hello3")
     public String Hello3() {
-        return "hello3";
+        return hello;
     }
 
 }
